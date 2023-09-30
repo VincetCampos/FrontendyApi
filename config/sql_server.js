6d4ece4b2690ea3fd6fe5b4ff0e2205fefac921b
vicente campos <vcamposs1@miumg.edu.gt>
2023-09-29 20:31:36 -0600

    primer commit

---
+++ config/sql_server.js
@@ -0,0 +1,12 @@
+
+const config = {
+    server: "localhost\\VINCET",
+    user: "sa",
+    password:"P@ss1234",
+    database: "Alumno",
+    options: {
+        trustServerCertificate: true
+    }
+}
+
+module.exports.config = config;
